8875034d6714efbf4f4720525b91704b4efee5d6
Markus S. Wamser <github-dev@mail2013.wamser.eu>
2026-09-17 15:04:15 +0200

    flake: drop flake-utils with minimal changes
    also drop Darwin support for scraper updater as this was never tested

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils }:
+  outputs = { self, nixpkgs }:
     let
       # use tools from given pkgs to extract the db from the download
       getDB = pkgs: pkgs.callPackage ./programs-sqlite.nix { inherit (nixpkgs) rev; };
@@ -12,15 +11,38 @@
       # shared NixOS & Home Manager module that extracts the db from its own `pkgs` instance
       # NB: this only works because the `command-not-found` options match exactly between NixOS & Home Manager
       sharedModule = pass@{ pkgs, ... }: (import ./module.nix { programs-sqlite = (getDB pkgs); }) pass;
+
+      linuxSystems = [ "aarch64-linux" "x86_64-linux" ];
+
+      # adopted from https://github.com/numtide/flake-utils/blob/11707dc2f618dd54ca8739b309ec4fc024de578b/lib.nix#L33
+      # which is also MIT licensed; can probably be further simplified
+      eachSystemOp = op: systems: f: builtins.foldl' (op f) { } systems;
+      eachSystem = eachSystemOp (
+        # Merge outputs for each system.
+        f: attrs: system:
+        let
+          ret = f system;
+        in
+        builtins.foldl' (
+          attrs: key:
+          attrs
+          // {
+            ${key} = (attrs.${key} or { }) // {
+              ${system} = ret.${key};
+            };
+          }
+        ) attrs (builtins.attrNames ret)
+      );
+      
     in
 
-    # provide db-package for all archs, but scaper/test only for most common
-    (utils.lib.eachSystem utils.lib.allSystems (system:
+    # provide db-package for all archs, but scraper/test only for common Linux
+    (eachSystem nixpkgs.lib.systems.doubles.all (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       nixpkgs.lib.recursiveUpdate
-        (nixpkgs.lib.optionalAttrs (builtins.elem system utils.lib.defaultSystems) rec {
+        (nixpkgs.lib.optionalAttrs (builtins.elem system linuxSystems) rec {
           packages.updater = pkgs.callPackage ./updater.nix {};
           apps.updater = { type = "app"; program = "${packages.updater}/bin/updater";};
           devShells.default = with pkgs; mkShell {
